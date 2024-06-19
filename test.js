f5d604531032489bfbf67d8a94dbead33feec68d
RiccardoGrimaudo <riccardo.grimaudo2002@gmail.com>
2024-06-19 10:10:13 +0200

    feat(api): all methods for users' routes have been added

--- test.js
+++ test.js
@@ -4,36 +4,44 @@ const data = fs.readFileSync("./activity-tracker-tester.json");
 const jsonData = JSON.parse(data);
 
 let token;
+let id;
+
+async function waitForToken() {
+  while (!token) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+}
+
+async function waitForId() {
+  while (!id) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
+}
 
 (async () => {
   try {
-    //Sezione login
-
-    //Body da passare per il login
-    const loginBody = {
-      // email: "ycnxdfsrvvapzsdsin@cazlq.com",
-      email: jsonData.users.admin.login.email,
-      // password: "1wVb|&WLr,kG3P[0{4?,c&IVcMihUkUk",
-      password: jsonData.users.admin.login.password,
-    };
-
-    //Chiamata alla api login
-    const loginReq = await fetch("http://127.0.0.1:3000/api/v1/users/login", {
+    let req = await fetch("http://127.0.0.1:3000/api/v1/users/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(loginBody),
+      body: JSON.stringify(jsonData.users.admin.login),
     });
 
-    //Conversione in json della chiamata
-    const loginRes = await loginReq.json();
+    let res = await req.json();
+    id = res.data.id;
 
-    //salvataggio del token
-    token = loginRes.token; // data.id per l'id
+    token = res.token;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
 
-    //Chiamata alla api getMe
-    const getMeReq = await fetch("http://127.0.0.1:3000/api/v1/users/getMe", {
+(async () => {
+  await waitForToken();
+
+  try {
+    let req = await fetch("http://127.0.0.1:3000/api/v1/users/getMe", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -41,12 +49,19 @@ let token;
       credentials: "include",
     });
 
-    //Conversione in json della chiamata
-    const getMeRes = await getMeReq.json();
+    let res = await req.json();
 
-    console.log(getMeRes);
+    console.log("Result of the getMe route:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
 
-    const getUsersReq = await fetch("http://127.0.0.1:3000/api/v1/users", {
+(async () => {
+  await waitForToken();
+
+  try {
+    let req = await fetch("http://127.0.0.1:3000/api/v1/users?limit=10", {
       method: "GET",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -54,29 +69,178 @@ let token;
       credentials: "include",
     });
 
-    const getUsersRes = await getUsersReq.json();
+    let res = await req.json();
 
-    for (let userId in getUsersRes) {
-      if (getUsersRes.hasOwnProperty(userId)) {
-        console.log(getUsersRes[userId]);
+    console.log("All users entered into the db:");
+    for (let userId in res) {
+      if (res.hasOwnProperty(userId)) {
+        console.log(res[userId]);
       }
     }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
 
-    const getUserByIdReq = await fetch(
-      `http://127.0.0.1:3000/api/v1/users/${loginRes.data.id}`,
+(async () => {
+  await waitForToken();
+  await waitForId();
+
+  try {
+    let req = await fetch(`http://127.0.0.1:3000/api/v1/users/${id}`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+    });
+
+    let res = await req.json();
+
+    console.log("Information about a specific user:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+
+  try {
+    let req = await fetch(`http://127.0.0.1:3000/api/v1/activities?limit=10`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      credentials: "include",
+    });
+
+    let res = await req.json();
+
+    console.log("All activities entered into the db:");
+    for (let userId in res) {
+      if (res.hasOwnProperty(userId)) {
+        console.log(res[userId]);
+      }
+    }
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+  await waitForId();
+
+  try {
+    let req = await fetch(
+      `http://127.0.0.1:3000/api/v1/users/${id}/activities`,
       {
         method: "GET",
         headers: {
           Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
         },
-        credentials: "include",
       }
     );
 
-    const getUserByIdRes = await getUserByIdReq.json();
+    let res = await req.json();
 
-    id = getUserByIdRes.data.id;
-    console.log(`ID: ${id}`);
+    console.log("Activities of a specific user:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+
+  try {
+    let req = await fetch(`http://127.0.0.1:3000/api/v1/users/updateMe`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData.users.admin.updateMe),
+    });
+
+    let res = await req.json();
+
+    console.log("Result of the updateMe route:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+
+  try {
+    let req = await fetch(
+      `http://127.0.0.1:3000/api/v1/users/updateMyPassword`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData.users.admin.updateMyPassword),
+      }
+    );
+
+    token = null;
+    let res = await req.json();
+
+    token = res.token;
+    console.log("Result of the updateMyPassword route:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+  await waitForId();
+
+  try {
+    let req = await fetch(
+      `http://127.0.0.1:3000/api/v1/users/changeStatus/${id}/?uri=123`,
+      {
+        method: "PATCH",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData.users.admin.changeStatus),
+      }
+    );
+
+    let res = await req.json();
+
+    console.log("Result of the changeStatus route:", res);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+})();
+
+(async () => {
+  await waitForToken();
+  await waitForId();
+
+  try {
+    let req = await fetch(`http://127.0.0.1:3000/api/v1/users/${id}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(jsonData.users.admin.userPatch),
+    });
+
+    let res = await req.json();
+
+    console.log("Result of the updateUser route:", res);
   } catch (error) {
     console.error("Error fetching data:", error);
   }
